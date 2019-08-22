1ce18d09c8a70589cfe050b2e1be3935e8d5e41b
A-Evseeva <evseeva@revival.ru>
2019-08-22 11:42:50 +0300

    function added for check if current user is task's controler

--- models/models.py
+++ models/models.py
@@ -131,13 +131,25 @@ class task(models.Model):
     # by default store = False this means the value of this field
     # is always computed.
     current_user = fields.Many2one('res.users', compute='_get_current_user')
+    isControler = fields.Boolean(compute='_is_controler')
+
+    def test_is_controler(self):
+        for rec in self:
+            result = (self.env.user.id == rec.controler_id.id)
+        return result
+
+    @api.depends('controler_id')
+    def _is_controler(self):
+        for rec in self:
+            rec.isControler = (self.env.user.id == rec.controler_id.id)
+
 
     @api.depends()
     def _get_current_user(self):
         for rec in self:
             rec.current_user = self.env.user
         # i think this work too so you don't have to loop
-        self.update({'current_user' : self.env.user.id})
+        self.update({'current_user' : self.env.user})
     
     @api.depends('asset_ids', 'compute_price_method', 'factor', 'tasktype_id')
     def _compute_price(self):
